0ef321334971d908936278d1ffaadce9f79251d9
torohkiss <torohkiss@yahoo.com>
2024-04-02 14:21:05 -0700

    tasj 3a

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,7 +10,6 @@ void binary_tree_delete(binary_tree_t *tree)
 	if (tree == NULL)
 		return;
 
-	binary_tree_delete(tree->parent);
 	binary_tree_delete(tree->left);
 	binary_tree_delete(tree->right);
 
