46a8e1c02e2cd92a975ddd25b1c349949b25123a
torohkiss <torohkiss@yahoo.com>
2024-04-03 07:29:37 -0700

    task 9a

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,14 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t lh, rh;
+	size_t lh = 0, rh = 0;
 
 	if (tree == NULL)
 		return (0);
-	else
-	{
-		lh = binary_tree_height(tree->left);
-		rh = binary_tree_height(tree->right);
-		return ((lh > rh ? lh : rh) + 1);
-	}
+
+	if (tree->left)
+		lh = 1 + binary_tree_height(tree->left);
+	if (tree->right)
+		rh = 1 + binary_tree_height(tree->right);
+	return ((lh > rh) ? lh : rh);
 }
